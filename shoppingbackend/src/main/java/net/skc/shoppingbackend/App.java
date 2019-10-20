9f7e8fa4d494ebc6f32d03c3ecfa1909475070a2
sundeepch1 <sundeep.chaurasiya@gmail.com>
2019-10-20 20:53:53 +0530

    project setup

---
+++ shoppingbackend/src/main/java/net/skc/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.skc.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
